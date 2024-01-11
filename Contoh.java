e18c0767b27ae8b8389a7f8334872fcf6d91d810
aldeco2008 <aldekoghanindra@gmail.com>
2024-01-11 09:25:47 +0700

    first commit

---
+++ Contoh.java
@@ -0,0 +1,10 @@
+public class Contoh {
+    public static void main(String[] args) {
+        double nilaiMat = 90;
+        double nilaiBI = 87.5;
+        double nilaiProd = 98.89;
+    
+        double hasil = (nilaiMat+nilaiBI+nilaiProd)/3;
+        System.out.println("Rata-rata Nilai: "+hasil);
+    }
+}
